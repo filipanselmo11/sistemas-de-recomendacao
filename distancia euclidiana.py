0849a02ba3cfcddc2a3e9947334eb9e6f0951a9f
filipanselmo11 <filip_anselmo@yahoo.com.br>
2018-09-11 10:56:33 -0400

    sistemas de recomendação usando distancia euclidiana

---
+++ distancia euclidiana.py
@@ -0,0 +1,55 @@
+from math import sqrt
+
+usuarios = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0,
+"Slighty Stoopid": 3.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+
+"Bill": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0,
+"Slighty Stoopid": 3.5, "Vampire Weekend": 2.0},
+
+"Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Dedmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5,
+"Slighty Stoopid": 1.0},
+
+"Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slighty Stoopid": 4.5,
+"The Strokes": 4.0, "Vampire Weekend": 2.0},
+
+"Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0,
+"Vampire Weekend": 1.0},
+
+"Jordyn": {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slighty Stoopid": 4.5,
+"The Strokes": 4.0, "Vampire Weekend": 4.0},
+
+"Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slighty Stoopid": 4.0,
+"The Strokes": 5.0},
+
+"Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slighty Stoopid": 2.5, "The Strokes": 3.0}}
+
+def euclidiana(avalicao1, avalicao2):
+    distancia = 0
+    for chave in avalicao1:
+        if chave in avalicao2:
+            potencia1 = pow(avalicao1[chave] - avalicao2[chave],2)
+            distancia = distancia + sqrt(potencia1)
+    return distancia
+
+def computaVizinho(nomeUsuario, usuarios):
+    distancias = []
+    for usuario in usuarios:
+        if usuario != nomeUsuario:
+            distancia = euclidiana(usuarios[usuario], usuarios[nomeUsuario])
+            distancias.append((distancia, usuario))
+    distancias.sort()
+    return distancias
+
+def recomendacao(nomeUsuario, usuarios):
+    vizinho = computaVizinho(nomeUsuario, usuarios)[0][1]
+    recomendacoes = []
+    avalicoesVizinho = usuarios[vizinho]
+    avalicaoesUsuario = usuarios[nomeUsuario]
+    for artista in avalicoesVizinho:
+        if not artista in avalicaoesUsuario:
+            recomendacoes.append((artista, avalicoesVizinho[artista]))
+    return sorted(recomendacoes, key = lambda Artista: Artista[1], reverse = True)
+
+
+print(euclidiana(usuarios["Veronica"], usuarios["Sam"]))
+print(recomendacao("Veronica",usuarios))
