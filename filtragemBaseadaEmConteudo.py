fe600852f1af31c30916f473cc4448707acef33e
FilipAnselmo <filip_anselmo@yahoo.com.br>
2018-10-30 19:19:27 -0400

    Add files via upload

---
+++ filtragemBaseadaEmConteudo.py
@@ -0,0 +1,12 @@
+import math
+import sqrt
+
+music = {"Dr.Dog/Fate":{"piano":2.5, "vocals":4, "beat":3.5,
+                        "blues":3, "guitar":5, "backup vocals": 4,
+                        "rap":1}
+        "Phoenix/Lisztomania":{"piano":2, "vocals":5, "beat":5,
+                               "blues":3, "guitar":2, "backup vocals":1,
+                               "rap":1,},
+        "Todd Snider/Don´t Tempt Me":{"piano":4, "vocals":, "beat":,
+                                      "blues":, "guitar": "backup vocals":,
+                                      "rap":}}
