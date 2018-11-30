e0937dfd03826b5b4a277653869b7ce8fa8db894
FilipAnselmo <filip_anselmo@yahoo.com.br>
2018-11-30 12:18:34 -0400

    Add files via upload

---
+++ filteringdata.py
@@ -0,0 +1,45 @@
+
+from math import sqrt
+
+
+musics = {"Dr Dog/Fate": {"piano": 2.5, "vocals": 4, "beat": 3.5, "blues": 3, "guitar": 5, "backup vocals": 4, "rap": 1},
+         "Phoenix/Lisztomania": {"piano": 2, "vocals": 5, "beat": 5, "blues": 3, "guitar": 2, "backup vocals": 1, "rap": 1},
+         "Heartless Bastards/Out at Sea": {"piano": 1, "vocals": 5, "beat": 4, "blues": 2, "guitar": 4, "backup vocals": 1, "rap": 1},
+         "Todd Snider/Don't Tempt Me": {"piano": 4, "vocals": 5, "beat": 4, "blues": 4, "guitar": 1, "backup vocals": 5, "rap": 1},
+         "The Black Keys/Magic Potion": {"piano": 1, "vocals": 4, "beat": 5, "blues": 3.5, "guitar": 5, "backup vocals": 1, "rap": 1},
+         "Glee Cast/Jessie's Girl": {"piano": 1, "vocals": 5, "beat": 3.5, "blues": 3, "guitar":4, "backup vocals": 5, "rap": 1},
+         "La Roux/Bulletproof": {"piano": 5, "vocals": 5, "beat": 4, "blues": 2, "guitar": 1, "backup vocals": 1, "rap": 1},
+         "Mike Posner": {"piano": 2.5, "vocals": 4, "beat": 4, "blues": 1, "guitar": 1, "backup vocals": 1, "rap": 1},
+         "Black Eyed Peas/Rock That Body": {"piano": 2, "vocals": 5, "beat": 5, "blues": 1, "guitar": 2, "backup vocals": 2, "rap": 4},
+         "Lady Gaga/Alejandro": {"piano": 1, "vocals": 5, "beat": 3, "blues": 2, "guitar": 1, "backup vocals": 2, "rap": 1}}
+
+
+userProfile = {"Fabio": {"piano": 2.5, "vocals": 4, "beat": 3.5, "blues": 3, "guitar": 5, "backup vocals": 4, "rap": 1}}
+
+
+
+
+def manhattan(music1, music2):
+    """Computes the Manhattan distance. """
+    distance = 0
+    total = 0
+    for key in music1:
+        if key in music2:
+            distance += abs(music1[key] - music2[key])
+            total += 1
+    return distance
+
+
+
+def recommend(userName, musics):
+    """creates a sorted list of muics based on their distance to musicName"""
+    distances = []
+    for music in musics:
+        distance = manhattan(musics[music], userProfile[userName])
+        distances.append((distance, music))
+    # sort based on distance -- closest first
+    distances.sort()
+    return distances
+
+
+
